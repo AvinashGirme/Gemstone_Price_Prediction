457602a2785b00a6ac81cca5a836df37b6532c69
AvinashGirme <girmeavinash717@gmail.com>
2023-04-17 15:00:53 +0530

    training and prediction code updated

--- src/utils.py
+++ src/utils.py
@@ -38,3 +38,13 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         logging.info('Exception Occured during model training')
         raise CustomException(e,sys)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info("Error occured during load object function")
+        raise CustomException(e,sys)
